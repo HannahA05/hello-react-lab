be98b0d15ca73a5f94d0cf8ed96006715ea429d0
HannahA05 <alkhate7@msu.edu>
2026-02-12 18:54:00 +0000

    Initial react app setup and edit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, React! 👋</h1>
+    </div>
+  )
+}
+
+export default App
